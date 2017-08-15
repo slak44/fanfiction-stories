9cc2aeac899b8cbb8bb9122c67367c055e0af6e5
Stefan Silviu <sylviu44@gmail.com>
2017-08-15 11:09:05 +0300

    Refactor model

--- app/src/main/java/slak/fanfictionstories/StoryListActivity.kt
+++ app/src/main/java/slak/fanfictionstories/StoryListActivity.kt
@@ -2,11 +2,9 @@ package slak.fanfictionstories
 
 import android.content.Context
 import android.os.Bundle
-import android.support.v4.view.GestureDetectorCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.CardView
 import android.util.AttributeSet
-import android.view.GestureDetector
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
@@ -14,8 +12,6 @@ import kotlinx.android.synthetic.main.activity_story_list.*
 import kotlinx.android.synthetic.main.content_story_list.*
 import kotlinx.android.synthetic.main.story_component.view.*
 import org.jetbrains.anko.db.*
-import java.text.SimpleDateFormat
-import java.util.*
 
 class StoryCardView : CardView {
   constructor(context: Context) : super(context)
@@ -60,83 +56,6 @@ class StoryCardView : CardView {
   }
 }
 
-enum class StoryStatus {
-  SEEN, REMOTE, LOCAL;
-
-  companion object {
-    fun fromString(s: String): StoryStatus = when (s) {
-      "seen" -> SEEN
-      "remote" -> REMOTE
-      "local" -> LOCAL
-      else -> throw IllegalArgumentException("Invalid arg: " + s)
-    }
-  }
-}
-
-class StoryModel(val src: Map<String, Any?>, val context: Context) {
-  val _id: Long = src["_id"] as Long
-  val storyidRaw: String = src["storyid"] as String
-
-  val title = src["title"] as String
-  val authorRaw = src["author"] as String
-  val summary = src["summary"] as String
-  val categoryRaw = src["category"] as String
-  val language = src["language"] as String
-  val genresRaw = src["genres"] as String
-  val charactersRaw = src["characters"] as String
-  val ratingRaw = src["rating"] as String
-  val status = StoryStatus.fromString(src["status"] as String) // FIXME: ui for this where
-  val wordCount: Int = (src["wordCount"] as Long).toInt()
-  val scrollProgress: Int = (src["scrollProgress"] as Long).toInt()
-  val chapterCount: Int = (src["chapters"] as Long).toInt()
-  val currentChapter: Int = (src["currentChapter"] as Long).toInt()
-  val reviewsCount: Int = (src["reviews"] as Long).toInt()
-  val favoritesCount: Int = (src["favorites"] as Long).toInt()
-  val followsCount: Int = (src["follows"] as Long).toInt()
-
-  // Processed data
-  val storyid: String get() = context.resources.getString(R.string.storyid_x, storyidRaw)
-  val isCompleted: Boolean get() = src["isCompleted"] as Long == 1L
-  val author: String get() = context.resources.getString(R.string.by_author, authorRaw)
-  val category: String get() = context.resources.getString(R.string.in_category, categoryRaw)
-  val words: String get() = context.resources.getString(R.string.x_words, wordCount)
-  val rating: String get() = context.resources.getString(R.string.rated_x, ratingRaw)
-  val genres: String get() = context.resources.getString(R.string.about_genres, genresRaw)
-  val characters: String get() = context.resources.getString(R.string.with_characters, charactersRaw)
-  val reviews: String get() = context.resources.getString(R.string.x_reviews, reviewsCount)
-  val favorites: String get() = context.resources.getString(R.string.x_favorites, favoritesCount)
-  val follows: String get() = context.resources.getString(R.string.x_follows, followsCount)
-  val progress: Int get() {
-    if (chapterCount == 1) return scrollProgress
-    // If this is too inaccurate, we might have to store each chapter's word count, then compute
-    // how far along we are
-    val avgWordCount: Float = wordCount.toFloat() / chapterCount
-    // amount of words before current chapter + amount of words scrolled through in current chapter
-    val wordsPassedEstimate: Float =
-        (currentChapter - 1) * avgWordCount + scrollProgress.toFloat() / 100 * avgWordCount
-    val percentPassed: Float = wordsPassedEstimate * 100 / wordCount
-    return Math.round(percentPassed)
-  }
-  val chapters: String get() {
-    // If we didn't start reading the thing, show total chapter count
-    if (currentChapter == 0) return context.resources.getString(R.string.x_chapters, chapterCount)
-    // Otherwise, list current chapter out of total
-    else return context.resources.getString(R.string.chapter_progress, currentChapter, chapterCount)
-  }
-
-  // Dates
-  val publishDateSeconds: Long = src["publishDate"] as Long
-  val updateDateSeconds: Long = src["updateDate"] as Long
-  val publishDateFormatted: String
-    get() = SimpleDateFormat.getDateInstance().format(Date(publishDateSeconds * 1000))
-  val updateDateFormatted: String
-    get() = SimpleDateFormat.getDateInstance().format(Date(updateDateSeconds * 1000))
-  val publishDate: String
-    get() = context.resources.getString(R.string.published_on, publishDateFormatted)
-  val updateDate: String
-    get() = context.resources.getString(R.string.updated_on, updateDateFormatted)
-}
-
 class StoryAdapter(val activity: StoryListActivity) : BaseAdapter() {
   var data = activity.getStoriesMeta()
 
@@ -149,7 +68,7 @@ class StoryAdapter(val activity: StoryListActivity) : BaseAdapter() {
   }
 
   override fun getItemId(position: Int): Long {
-    return data[position]._id
+    return data[position]._id.get()
   }
 
   override fun getView(idx: Int, recycleView: View?, parent: ViewGroup?): View {
@@ -166,7 +85,7 @@ class StoryListActivity : AppCompatActivity() {
       select(tableName = "stories").exec {
         parseList(object : MapRowParser<StoryModel> {
           override fun parseRow(columns: Map<String, Any?>) =
-              StoryModel(columns, this@StoryListActivity)
+              StoryModel(columns, this@StoryListActivity, fromDb = true)
         })
       }
     }
---
+++ app/src/main/java/slak/fanfictionstories/StoryModel.kt
@@ -0,0 +1,83 @@
+package slak.fanfictionstories
+
+import android.content.Context
+import java.text.SimpleDateFormat
+import java.util.*
+
+enum class StoryStatus {
+  SEEN, REMOTE, LOCAL;
+
+  companion object {
+    fun fromString(s: String): StoryStatus = when (s) {
+      "seen" -> SEEN
+      "remote" -> REMOTE
+      "local" -> LOCAL
+      else -> throw IllegalArgumentException("Invalid arg: " + s)
+    }
+  }
+}
+
+class StoryModel(val src: Map<String, Any?>, val context: Context, fromDb: Boolean) {
+  // DB primary key. Does not exist if story not from db
+  var _id: Optional<Long> = if (fromDb) Optional.of(src["_id"] as Long) else Optional.empty()
+
+  val storyidRaw: String = src["storyid"] as String
+  val title = src["title"] as String
+  val authorRaw = src["author"] as String
+  val summary = src["summary"] as String
+  val categoryRaw = src["category"] as String
+  val language = src["language"] as String
+  val genresRaw = src["genres"] as String
+  val charactersRaw = src["characters"] as String
+  val ratingRaw = src["rating"] as String
+  val status = StoryStatus.fromString(src["status"] as String) // FIXME: ui for this where
+  val wordCount: Int = (src["wordCount"] as Long).toInt()
+  val scrollProgress: Int = (src["scrollProgress"] as Long).toInt()
+  val chapterCount: Int = (src["chapters"] as Long).toInt()
+  val currentChapter: Int = (src["currentChapter"] as Long).toInt()
+  val reviewsCount: Int = (src["reviews"] as Long).toInt()
+  val favoritesCount: Int = (src["favorites"] as Long).toInt()
+  val followsCount: Int = (src["follows"] as Long).toInt()
+
+  // Processed data
+  val storyid: String get() = context.resources.getString(R.string.storyid_x, storyidRaw)
+  val isCompleted: Boolean get() = src["isCompleted"] as Long == 1L
+  val author: String get() = context.resources.getString(R.string.by_author, authorRaw)
+  val category: String get() = context.resources.getString(R.string.in_category, categoryRaw)
+  val words: String get() = context.resources.getString(R.string.x_words, wordCount)
+  val rating: String get() = context.resources.getString(R.string.rated_x, ratingRaw)
+  val genres: String get() = context.resources.getString(R.string.about_genres, genresRaw)
+  val characters: String get() = context.resources.getString(R.string.with_characters, charactersRaw)
+  val reviews: String get() = context.resources.getString(R.string.x_reviews, reviewsCount)
+  val favorites: String get() = context.resources.getString(R.string.x_favorites, favoritesCount)
+  val follows: String get() = context.resources.getString(R.string.x_follows, followsCount)
+  val progress: Int get() {
+    if (chapterCount == 1) return scrollProgress
+    // If this is too inaccurate, we might have to store each chapter's word count, then compute
+    // how far along we are
+    val avgWordCount: Float = wordCount.toFloat() / chapterCount
+    // amount of words before current chapter + amount of words scrolled through in current chapter
+    val wordsPassedEstimate: Float =
+        (currentChapter - 1) * avgWordCount + scrollProgress.toFloat() / 100 * avgWordCount
+    val percentPassed: Float = wordsPassedEstimate * 100 / wordCount
+    return Math.round(percentPassed)
+  }
+  val chapters: String get() {
+    // If we didn't start reading the thing, show total chapter count
+    if (currentChapter == 0) return context.resources.getString(R.string.x_chapters, chapterCount)
+    // Otherwise, list current chapter out of total
+    else return context.resources.getString(R.string.chapter_progress, currentChapter, chapterCount)
+  }
+
+  // Dates
+  val publishDateSeconds: Long = src["publishDate"] as Long
+  val updateDateSeconds: Long = src["updateDate"] as Long
+  val publishDateFormatted: String
+    get() = SimpleDateFormat.getDateInstance().format(Date(publishDateSeconds * 1000))
+  val updateDateFormatted: String
+    get() = SimpleDateFormat.getDateInstance().format(Date(updateDateSeconds * 1000))
+  val publishDate: String
+    get() = context.resources.getString(R.string.published_on, publishDateFormatted)
+  val updateDate: String
+    get() = context.resources.getString(R.string.updated_on, updateDateFormatted)
+}
