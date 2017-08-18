f9b99b60a7f6a928c88dafd124195016be22fc56
Stefan Silviu <sylviu44@gmail.com>
2017-08-18 21:42:33 +0300

    Implement support for <hr> in reader

--- app/src/main/java/slak/fanfictionstories/StoryReaderActivity.kt
+++ app/src/main/java/slak/fanfictionstories/StoryReaderActivity.kt
@@ -1,9 +1,12 @@
 package slak.fanfictionstories
 
+import android.graphics.Canvas
+import android.graphics.Paint
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.text.Html
+import android.text.style.ReplacementSpan
 import kotlinx.android.synthetic.main.activity_story_reader.*
 import kotlinx.android.synthetic.main.content_story_reader.*
 import kotlinx.coroutines.experimental.CommonPool
@@ -14,10 +17,30 @@ import kotlinx.coroutines.experimental.launch
 import org.jetbrains.anko.db.update
 import java.io.File
 
+class HrSpan(val heightPx: Int, val width: Int) : ReplacementSpan() {
+  override fun getSize(p0: Paint?, p1: CharSequence?, p2: Int, p3: Int,
+                       p4: Paint.FontMetricsInt?): Int {
+    return 0
+  }
+
+  override fun draw(canvas: Canvas, text: CharSequence, start: Int, end: Int, x: Float, top: Int,
+                    y: Int, bottom: Int, paint: Paint) {
+    canvas.drawRect(x, top.toFloat(), (y + width).toFloat(), (top + heightPx).toFloat(), paint)
+  }
+}
+
 class StoryReaderActivity : AppCompatActivity() {
 
   companion object {
     const val INTENT_STORY_MODEL = "bundle"
+    private val PLACEHOLDER = "######HRPLACEHOLDERHRPLACEHOLDERHRPLACEHOLDER######"
+    private val getTagHandler = { widthPx: Int -> Html.TagHandler { opening, tag, output, _ ->
+      if (tag == "hr") {
+        if (opening) output.insert(output.length, PLACEHOLDER)
+        else output.setSpan(HrSpan(1, widthPx),
+            output.length - PLACEHOLDER.length, output.length, 0)
+      }
+    } }
   }
 
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,7 +61,9 @@ class StoryReaderActivity : AppCompatActivity() {
     launch(CommonPool) {
       val text = readChapter(model.storyidRaw, chapterToRead).await()
       launch(UI) {
-        chapterText.text = Html.fromHtml(text, Html.FROM_HTML_MODE_LEGACY)
+        // Legacy mode puts more space between <p>, makes it easier to read
+        chapterText.text =
+            Html.fromHtml(text, Html.FROM_HTML_MODE_LEGACY, null, getTagHandler(chapterText.width))
         // FIXME reinstate scroll
       }
       database.use {
