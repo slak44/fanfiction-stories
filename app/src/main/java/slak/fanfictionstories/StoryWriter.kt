513a214f44916a2419e240cac3ae3cecb14a2fad
Stefan Silviu <sylviu44@gmail.com>
2017-08-18 20:56:49 +0300

    Refactor writer

--- app/src/main/java/slak/fanfictionstories/StoryWriter.kt
+++ app/src/main/java/slak/fanfictionstories/StoryWriter.kt
@@ -29,42 +29,51 @@ fun errorDialog(ctx: Context, title: String, msg: String) = launch(UI) {
 
 fun haveExternalStorage() = Environment.MEDIA_MOUNTED == Environment.getExternalStorageState()
 
+/**
+ * @returns a File representing the external storage dir, or Optional.empty() if it's unavailable
+ */
 fun getStorageDir(ctx: Context): Optional<File> =
     if (haveExternalStorage()) Optional.of(ctx.getExternalFilesDir(null)) else Optional.empty()
 
-fun getStoriesDir(ctx: Context): Optional<File> = if (haveExternalStorage())
-  Optional.of(File(getStorageDir(ctx).get(), "storiesData")) else Optional.empty()
+/**
+ * @returns a File representing the stories dir, or Optional.empty() if it's unavailable
+ */
+fun storyDir(ctx: Context, storyid: Long): Optional<File> {
+  val storage = getStorageDir(ctx)
+  if (!storage.isPresent) {
+    Log.e("StoryWriter#storyDir", "no ext storage")
+    errorDialog(ctx, R.string.ext_store_unavailable, R.string.ext_store_unavailable_tip)
+    return Optional.empty()
+  }
+  val storiesDir = File(storage.get(), "storiesData")
+  return Optional.of(File(storiesDir, storyid.toString()))
+}
 
 /**
  * Writes received story data to disk
  * @returns true if we started writing data to disk, false otherwise
  */
 fun writeStory(ctx: Context, storyid: Long, chapters: Channel<String>): Boolean {
-  val storiesDir = getStoriesDir(ctx)
-  if (!storiesDir.isPresent) {
-    Log.e("StoryWriter", "no ext storage")
-    errorDialog(ctx, R.string.ext_store_unavailable, R.string.ext_store_unavailable_tip)
-    return false
-  }
-  val targetDir = File(storiesDir.get(), storyid.toString())
-  if (targetDir.exists()) {
+  val targetDir = storyDir(ctx, storyid)
+  if (!targetDir.isPresent) return false
+  if (targetDir.get().exists()) {
     // FIXME maybe ask the user if he wants to overwrite or legitimize this by getting the metadata
     Log.e("StoryWriter", "targetDir exists")
     errorDialog(ctx, R.string.storyid_already_exists, R.string.storyid_already_exists_tip)
     return false
   }
-  val madeDirs = targetDir.mkdirs()
+  val madeDirs = targetDir.get().mkdirs()
   if (!madeDirs) {
     Log.e("StoryWriter", "can't make dirs")
     errorDialog(ctx,
         ctx.resources.getString(R.string.failed_making_dirs),
-        ctx.resources.getString(R.string.failed_making_dirs_tip, targetDir.absolutePath))
+        ctx.resources.getString(R.string.failed_making_dirs_tip, targetDir.get().absolutePath))
     return false
   }
   launch(CommonPool) {
     var idx: Int = 1
     chapters.consumeEach { chapterText: String ->
-      File(targetDir, "$idx.html").printWriter().use { it.print(chapterText) }
+      File(targetDir.get(), "$idx.html").printWriter().use { it.print(chapterText) }
       idx++
     }
   }
