2468debdc3dcf9283e392d29868d794783a7c368
Stefan Silviu <sylviu44@gmail.com>
2017-08-18 21:46:52 +0300

    Remove unused import

--- app/src/main/java/slak/fanfictionstories/StoryModel.kt
+++ app/src/main/java/slak/fanfictionstories/StoryModel.kt
@@ -1,7 +1,6 @@
 package slak.fanfictionstories
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.os.Bundle
 import android.os.Parcel
 import android.os.Parcelable
