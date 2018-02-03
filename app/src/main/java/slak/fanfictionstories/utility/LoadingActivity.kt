f068593012d6cbeca95afd57714e90ba9e1be800
Stefan Silviu <sylviu44@gmail.com>
2018-02-03 15:30:22 +0200

    Rename file

--- app/src/main/java/slak/fanfictionstories/utility/LoadingActivity.kt
+++ app/src/main/java/slak/fanfictionstories/utility/LoadingActivity.kt
@@ -3,14 +3,14 @@ package slak.fanfictionstories.utility
 import android.support.v7.widget.Toolbar
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.loading_progress_bar.*
+import kotlinx.android.synthetic.main.loading_activity_indeterminate.*
 import slak.fanfictionstories.R
 
 open class LoadingActivity(private val idxInToolbarLayout: Int = -1) : ActivityWithStatic() {
   override fun setSupportActionBar(toolbar: Toolbar?) {
     super.setSupportActionBar(toolbar)
     val progress = layoutInflater.inflate(
-        R.layout.loading_progress_bar, toolbar!!.parent as ViewGroup, false)
+        R.layout.loading_activity_indeterminate, toolbar!!.parent as ViewGroup, false)
     (toolbar.parent as ViewGroup).addView(progress, idxInToolbarLayout)
   }
   fun showLoading() {
