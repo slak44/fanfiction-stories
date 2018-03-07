c248fd5a46b9faa090655e7f1960143e28cdb23e
Stefan Silviu <sylviu44@gmail.com>
2018-03-07 19:01:56 +0200

    Remove unused import

--- app/src/main/java/slak/fanfictionstories/activities/AuthorActivity.kt
+++ app/src/main/java/slak/fanfictionstories/activities/AuthorActivity.kt
@@ -1,6 +1,5 @@
 package slak.fanfictionstories.activities
 
-import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.TabLayout
 import android.support.v4.app.Fragment
